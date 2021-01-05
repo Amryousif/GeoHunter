ee0fc6c6e94f27864b882ba8e8b103357a2f90dd
tineryum <74090232+tineryum@users.noreply.github.com>
2021-01-05 16:08:46 +0300

    Add files via upload

---
+++ GeoHunter.py
@@ -0,0 +1,42 @@
+from requests import get
+from sys import argv, platform
+from os import system
+
+def clear():
+    if platform == "linux":
+        system("clear")
+    elif platform == "win32":
+        system("cls")
+
+clear()
+
+print(""" 
+  ____            _   _             _            
+ / ___| ___  ___ | | | |_   _ _ __ | |_ ___ _ __ 
+| |  _ / _ \/ _ \| |_| | | | | '_ \| __/ _ \ '__|
+| |_| |  __/ (_) |  _  | |_| | | | | ||  __/ |   
+ \____|\___|\___/|_| |_|\__,_|_| |_|\__\___|_|   
+    
+    Coded by tineryum
+    Github : tineryum
+    Mail   : tineryum@protonmail.com
+                                              
+""")
+
+try:
+    ip = argv[1]
+    infoGet = get("http://ip-api.com/json/{}".format(ip))
+    info = infoGet.json()
+    print(""" 
+        
+       Country     : {}
+       City         : {}
+       Country Code  : {}
+       Zip Code     : {}
+       Isp         : {}       
+
+        """.format(info['country'],info['city'],info['countryCode'],info['zip'],info['isp']))                      
+            
+except:
+    clear()
+    print("Usage : python3 GeoHunter.py <target_ip>") 
